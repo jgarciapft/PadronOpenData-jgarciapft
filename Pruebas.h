8e8b175148e025c6d10720f32f78eb23107ebddf
jgarciapft <jgarciapft@alumnos.unex.es>
2018-05-19 10:55:39 +0200

    ARCHIVOS:     +Añadido el set de datos 1 @SetDatos_1_*     *Modificado el .gitignore para incluir en el repositorio la clase Utils
    Clase Utils:
        *N_CAMPOS_DATOS_DEMOGRAFICOS = 8 (era 7)
        +Añadidas las constantes para la prueba del algoritmo 1
    
    Clase Padron;
        *Corregida la carga de datos demográficos. No leía toda la línea porque mezclaba el último campo con el del año de padron. Ahora el vector de campos lo almacena pero no lo usa
    
    Clase Pruebas:
        * Corregida la definición de las pruebas del algoritmo 1

--- Pruebas.h
+++ Pruebas.h
@@ -139,7 +139,7 @@ private:
 	 */
 	void pruebaEjecucionSecuencial();
 
-	/**@PRUEBA	Validación de la carga de datos a partir del set de datos \SET_DATOS: SET_1
+	/**@PRUEBA	Validación de la carga de datos a partir del set de datos \SET_DATOS: Set_1
 	 * 				-Un fichero con los BARRIOS. 			RUTA_BARRIO
 	 * 				-Un fichero con las VÍAS.				RUTA_VIA
 	 * 				-Un fichero con los datos del PADRON.	RUTA_PADRON
@@ -147,7 +147,7 @@ private:
 	 * @ESTRUCTURA: Lista de vías auxiliar 'lVias' en la clase 'Padron'
 	 *  		1. No existe el fichero RUTA_VIA		->	\return = false
 	 *  		2. Existe el fichero RUTA_VIA
-	 *  			* Validación por conteo de objetos	->	cont = 5 y \return = true
+	 *  			* Validación por conteo de objetos	->	cont = 4 y \return = true
 	 *
 	 * @ESTRUCTURA: ABB de vías auxiliar 'aVias' en la clase 'Padron'
 	 *  		1. Deductivamente si los métodos auxiliares son correctos y si se carga correctamente 'lVias': 'aVias' también es correcto
@@ -155,16 +155,17 @@ private:
 	 * @ESTRUCTURA: Lista de datos demográficos auxiliar 'lDatDemograficos' en la clase 'Padron'
 	 *  		1. No existe el fichero RUTA_PADRON		->  \return = false
 	 *  		2. Existe el fichero RUTA_PADRON
-	 *  			* Validación por conteo de objetos	->	cont = 4 y \return = true
+	 *  			* Validación por conteo de objetos	->	cont = 3 y \return = true
 	 *
 	 * @ESTRUCTURA: Estructura de datos según en diagrama UML a partir de 'gBarrio' en la clase 'Padron'
 	 * 			1. No existe ningún fichero de datos							->	\return = false
 	 * 			2. Existen todos los ficheros de datos							->	\return = true
-	 * 				2.1 'lBarrios' contiene 3 barrios							->	BarrioA, BarrioB, BarrioC
+	 * 				2.1 'lBarrios' contiene 3 barrios (2 + barrio contenedor)	->	BarrioA, BarrioB, BARRIO_CONTENEDOR
 	 * 				2.2 El barrio 'BarrioA' contiene 1 vía 						->	ViaA
-	 * 				2.3 El barrio 'BarrioA' contiene más de 1 vía 				-> 	Deductivamente si el algoritmo sirve para 1 vía, sirve para 'n' vías
-	 * 				2.4 La vía 'ViaA' no contiene un set de datos demográficos	->	No se muestra nada
-	 * 				2.5 La vía 'ViaA' contiene un set de datos demográficos		->	[AñoNacA, LugNacA, EstudA, NacionA]
+	 * 				2.3 El barrio 'BarrioB' contiene más de 1 vía 				-> 	Deductivamente si el algoritmo sirve para 1 vía, sirve para 'n' vías
+	 * 				2.4 El barrio contenedor contiene una vía					->	ViaContenedor
+	 * 				2.5 La vía 'ViaA' no contiene un set de datos demográficos	->	No se muestra nada
+	 * 				2.6 La vía 'ViaB' contiene un set de datos demográficos		->	[AñoNacB, LugNacB, EstudB, NacionB]
 	 */
 	void pruebaEstructuraLVias();
 	void pruebaEstructuraLDatDem();
---
+++ Utils.h
@@ -0,0 +1,64 @@
+//============================================================================
+// Name        : Utils.h
+// Author      : Profesores de la asignatura EDI
+// Version     : Curso 17/18
+// Copyright   :
+// Description : Utilidades para trocear cadenas
+//============================================================================
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+/****************************************************** DEFINCIÓN DE CONSTANTES ******************************************************/
+
+const string RUTA_BARRIO = "Barrio.csv"; 							//Ruta relativa del fichero que contiene la información de los barrios
+const string RUTA_VIAS = "Via.csv"; 								//Ruta relativa del fichero que contiene la información de las vías
+const string RUTA_PADRON = "InformacionPadron.csv";	 				//Ruta relativa del fichero que contiene la informacion del padrón (Año 2016)
+const string RUTA_FIHCERO_PRUEBAS = "Pruebas.txt";					//Ruta relativa del fichero de volcado para la ejecución de las pruebas
+const int N_CAMPOS_BARRIO = 2; 										//Número de campos del fichero de datos de barrios
+const int N_CAMPOS_VIA = 5;											//Número de campos del fichero de datos de vías
+const int N_CAMPOS_DATOS_DEMOGRAFICOS = 8;							//Número de campos del fichero de datos de datos demográficos (Contando el año del padron)
+const char SEP = '#'; 												//Carácter separador de campos en los ficheros de datos
+const char DELIM = ';';												//Carácter separador de campos para datos del fichero de información sobre el padrón
+const string DEF_BARRIO = "BARRIO CONTENEDOR";						//Nombre del distrito del objeto 'Barrio' que incluye las vías que no tienen ningún barrio asignado
+const string DEF_PLACEHOLDER = "";									//Nombre del distrito del objeto 'Barrio' que incluye las vías que no tienen ningún barrio asignado
+const int RANGO_EDAD_ALG_4 = 10;									//Amplitud del intervalo de edad que usa el algorimto 4 para agrupar los años de nacimiento. Por defecto son décadas
+const string POBLACION_TEXTO_RELLENO = "";							//Texto de relleno para el atributo 'poblacion' al crear nuevos objetos 'LugarNacimiento' aplicado al algoritmo 6
+const string PROVINCIA_PAIS_TEXTO_RELLENO = "";						//Texto de relleno para el atributo 'provincia/pais' al crear nuevos objetos 'LugarNacimiento' aplicado al algoritmo 10
+const char TECLA_MENU = 'm';										//Tecla para volver al menú de la interfaz del usuario
+
+/******************************************************* CONSTANTES DE PRUEBAS *******************************************************/
+
+///@TEST: Pruebas del algoritmo 1 (Carga de datos)	\SET_DATOS: Set_1
+const string RUTA_BARRIO_PRUEBA_ALG1 = "SetDatos_1_Barrios.csv";
+const string RUTA_VIA_PRUEBA_ALG1 = "SetDatos_1_Vias.csv";
+const string RUTA_PADRON_PRUEBA_ALG1 = "SetDatos_1_Padron.csv";
+
+/****************************************************** DEFINCIÓN DE CONSTANTES ******************************************************/
+
+/*
+ * PRE: La cadena pasada por parámetros no debe estar vacía
+ * POST: Divide la cadena Text en subcadenas en base al delimitador 'delim'. Las distintas subcadenas son proporcionadas en un vector
+ * Ejemplo: "1958 (5); 1988 (3)" -> |1958 (5)| 1988 (3)|
+ * COMPLEJIDAD: O(n)
+ */
+vector<string> splitStringToVector(string text, char delim);
+
+/*
+ * PRE:La cadena de caracteres pasada por parámetros no debe estar vacía. Text puede contener distintos valores entre par�ntesis.
+ * POST: Divide la cadena 'text' en subcadenas. Elimina los caracteres '(' y ')' y devuelve cada valor en una casilla del vector.
+ * Ejemplo: "1958 (5)" -> |1958|5|
+ * COMPLEJIDAD: O(n)
+ */
+vector<string> splitString(string text);
+
+std::string int_to_string(int n);
+
+#endif /* UTILS_H_ */
